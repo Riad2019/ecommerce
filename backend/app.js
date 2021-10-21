26ae4391bbbd6d8ba6879953680e5393aae2043e
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-10-22 05:42:18 +0600

    done

--- backend/app.js
+++ backend/app.js
@@ -28,9 +28,7 @@ app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.get('/',(req, res) => {
-  res.send("hello from db it's working");
-})
+
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
